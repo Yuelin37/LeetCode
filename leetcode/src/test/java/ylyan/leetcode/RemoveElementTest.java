6fc601565ad4f164d06a4994d98df1d551d06f4a
Yuelin37 <yuelinyan@gmail.com>
2015-01-23 17:42:48 +0800

    nothing

--- leetcode/src/test/java/ylyan/leetcode/RemoveElementTest.java
+++ leetcode/src/test/java/ylyan/leetcode/RemoveElementTest.java
@@ -2,33 +2,38 @@ package ylyan.leetcode;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
+
+
 import junit.framework.TestCase;
 
 public class RemoveElementTest extends TestCase {
+	private RemoveElement re;
 
 	Logger logger = Logger.getLogger("ylyan.leetcode");
 	{
 		PropertyConfigurator.configure("log.conf");
-
 	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		re = new RemoveElement();
+	}
+
 	public void testRemoveElement() {
-		
-		RemoveElement re = new RemoveElement();
-		int[] A = { 7,12,1, 2, 3, 4, 5, 5, 5, 33, 3 };
+
+		int[] A = { 7, 12, 1, 2, 3, 4, 5, 5, 5, 33, 3 };
 		int elem = 5;
 		assertEquals(8, re.removeElement(A, elem));
 	}
-	
-	public void testEmptyArray(){
-		RemoveElement re = new RemoveElement();
-		int[] A={};
+
+	public void testEmptyArray() {
+		int[] A = {};
 		int elem = 5;
 		assertEquals(0, re.removeElement(A, elem));
 	}
-
-	public void testOneElementArray(){
-		RemoveElement re = new RemoveElement();
-		int[] A={1};
+	
+	public void testOneElementArray() {
+		int[] A = { 1 };
 		int elem = 1;
 		assertEquals(0, re.removeElement(A, elem));
 	}
