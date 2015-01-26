573f9462546f07925515975ba4c151e8afe5e0d7
Yuelin37 <yuelinyan@gmail.com>
2015-01-26 18:06:26 +0800

    Refactor and add a BaseTestCase class. Add log4j logger initialization to BaseTestCase.

---
+++ leetcode/src/test/java/ylyan/leetcode/BaseTestCase.java
@@ -0,0 +1,19 @@
+package ylyan.leetcode;
+
+import junit.framework.TestCase;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public abstract class BaseTestCase extends TestCase{
+	protected Logger logger = Logger.getLogger("ylyan.leetcode");
+	
+	protected void setUp(){
+		PropertyConfigurator.configure("log.conf");
+	}
+	
+	public BaseTestCase(){
+//		PropertyConfigurator.configure("log.conf");
+	}
+
+}
--- leetcode/src/test/java/ylyan/leetcode/MergeSortedArrayTest.java
+++ leetcode/src/test/java/ylyan/leetcode/MergeSortedArrayTest.java
@@ -2,79 +2,78 @@ package ylyan.leetcode;
 
 import java.util.Arrays;
 
-import junit.framework.TestCase;
-
-public class MergeSortedArrayTest extends TestCase {
+public class MergeSortedArrayTest extends BaseTestCase {
 
 	private MergeSortedArray msa;
 
-	protected void setUp() throws Exception {
+	protected void setUp() {
 		super.setUp();
 		msa = new MergeSortedArray();
 	}
 
 	protected void tearDown() throws Exception {
 		super.tearDown();
+//		logger.debug("haha");
 	}
 
-	 public void testMerge() {
-	 int m = 3;
-	 int n = 4;
-	 int[] A = new int[m+n];
-	 int[] B = new int[n];
-	
-	 A[0] = 1;
-	 A[1] = 3;
-	 A[2] = 5;
-	 B[0] = 2;
-	 B[1] = 4;
-	 B[2] = 6;
-	 B[3] = 7;
-	
-	 msa.merge(A, m, B, n);
-	 System.out.println("----------------" + Arrays.toString(A));
-	 }
-	//
-	 public void testEmptyA() {
-	 int m = 0;
-	 int n = 1;
-	 int[] A = new int[m+n];
-	 int[] B = new int[n];
-	
-	 B[0] = 1;
-	
-	 msa.merge(A, m, B, n);
-	 System.out.println("----------------" + Arrays.toString(A));
-	 }
-	
-	 public void testMerge1() {
-	 int m = 3;
-	 int n = 3;
-	 int[] A = new int[m+n];
-	 int[] B = new int[n];
-	 A[0] = 1;
-	 A[1] = 2;
-	 A[2] = 4;
-	 B[0] = 3;
-	 B[1] = 5;
-	 B[2] = 6;
-	
-	 msa.merge(A, m, B, n);
-	 System.out.println("----------------" + Arrays.toString(A));
-	 }
-	//
-	 public void testMerge2() {
-	 int m = 1;
-	 int n = 1;
-	 int[] A = new int[m+n];
-	 int[] B = new int[n];
-	
-	 A[0] = 1;
-	 B[0] = 2;
-	
-	 msa.merge(A, m, B, n);
-	 System.out.println("----------------" + Arrays.toString(A));
-	 }
+	public void testMerge() {
+		int m = 3;
+		int n = 4;
+		int[] A = new int[m + n];
+		int[] B = new int[n];
+
+		A[0] = 1;
+		A[1] = 3;
+		A[2] = 5;
+		B[0] = 2;
+		B[1] = 4;
+		B[2] = 6;
+		B[3] = 7;
+
+		msa.merge(A, m, B, n);
+		assertTrue(Arrays.equals(A, new int[] { 1, 2, 3, 4, 5, 6,7 }));
+	}
+
+	public void testEmptyA() {
+		int m = 0;
+		int n = 1;
+		int[] A = new int[m + n];
+		int[] B = new int[n];
+
+		B[0] = 1;
+
+		msa.merge(A, m, B, n);
+		assertTrue(Arrays.equals(A, new int[] { 1 }));
+	}
+
+	public void testMerge1() {
+		int m = 3;
+		int n = 3;
+		int[] A = new int[m + n];
+		int[] B = new int[n];
+		A[0] = 1;
+		A[1] = 2;
+		A[2] = 4;
+		B[0] = 3;
+		B[1] = 5;
+		B[2] = 6;
+
+		msa.merge(A, m, B, n);
+		assertTrue(Arrays.equals(A, new int[] { 1, 2, 3, 4, 5, 6 }));
+	}
+
+	public void testMerge2() {
+		int m = 1;
+		int n = 1;
+		int[] A = new int[m + n];
+		int[] B = new int[n];
+
+		A[0] = 1;
+		B[0] = 2;
+
+		msa.merge(A, m, B, n);
+		assertTrue(Arrays.equals(A, new int[] { 1, 2 }));
+	}
 
 	public void testMerge3() {
 		int m = 3;
@@ -89,9 +88,9 @@ public class MergeSortedArrayTest extends TestCase {
 		B[2] = 3;
 
 		msa.merge(A, m, B, n);
-		System.out.println("----------------" + Arrays.toString(A));
+		assertTrue(Arrays.equals(A, new int[] { 1, 2, 3, 4, 5, 6 }));
 	}
-	
+
 	public void testMerge4() {
 		int m = 3;
 		int n = 3;
@@ -105,7 +104,7 @@ public class MergeSortedArrayTest extends TestCase {
 		B[2] = 6;
 
 		msa.merge(A, m, B, n);
-		System.out.println("----------------" + Arrays.toString(A));
+		assertTrue(Arrays.equals(A, new int[] { 1, 2, 3, 4, 5, 6 }));
 	}
 
 }
