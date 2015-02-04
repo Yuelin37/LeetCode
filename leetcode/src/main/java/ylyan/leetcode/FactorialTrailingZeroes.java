15412ee87ec00ce04a652060137e481c17abdef8
Yuelin Yan <yuelinyan@gmail.com>
2015-02-04 13:48:10 +0800

    172. Factorial Trailing Zeroes

---
+++ leetcode/src/main/java/ylyan/leetcode/FactorialTrailingZeroes.java
@@ -0,0 +1,15 @@
+/**
+ * 172. Factorial Trailing Zeroes
+ * Given an integer n, return the number of trailing zeroes in n!.
+ * Note: Your solution should be in logarithmic time complexity.
+ * 
+ * Credits:
+ * Special thanks to @ts for adding this problem and creating all test cases.
+ */
+package ylyan.leetcode;
+
+public class FactorialTrailingZeroes {
+	public int trailingZeroes(int n) {
+		return 0;
+	}
+}
---
+++ leetcode/src/test/java/ylyan/leetcode/FactorialTrailingZeroesTest.java
@@ -0,0 +1,19 @@
+package ylyan.leetcode;
+
+import junit.framework.TestCase;
+
+public class FactorialTrailingZeroesTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	public void testTrailingZeroes(){
+		
+	}
+
+}
