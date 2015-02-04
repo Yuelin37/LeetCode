e3e9e06ba590a488ca39fe886fed04e8b8da6239
Yuelin37 <yuelinyan@gmail.com>
2015-02-04 13:59:29 +0800

    CompareVersionNumber

--- leetcode/src/main/java/ylyan/leetcode/MajorityElement.java
+++ leetcode/src/main/java/ylyan/leetcode/MajorityElement.java
@@ -11,51 +11,61 @@ import java.util.Arrays;
 
 public class MajorityElement {
 	public int majorityElement(int[] num) {
-//		int n = num.length;
-//		int cur = 0;
-//		int majority = 0, majorCount = 0;
-//		int[] tmp = new int[n / 2 + 1];
-//		int[] tmpCount = new int[n / 2 + 1];
-//		tmp[0] = num[0];
-//
-//		boolean added = false;
-//
-//		for (int i = 0; i < n; i++) {
-//			added = false;
-//			for (int j = 0; j <= cur; j++) {
-//				if (num[i] == tmp[j]) {
-//					added = true;
-//					tmpCount[j]++;
-//					break;
-//				}
-//			}
-//			if (!added) {
-//				cur++;
-//				tmp[cur] = num[i];
-//				tmpCount[cur]++;
-//			}
-//		}
-//
-//		majority = tmp[0];
-//		majorCount = 0;
-//		for (int i = 0; i < tmp.length; i++) {
-//			if (tmpCount[i] > majorCount) {
-//				majorCount = tmpCount[i];
-//				majority = tmp[i];
-//			}
-//		}
-//		return majority;
-		
+
+		return (method1Pass(num));
+	}
+
+	public int leedcodeSolution(int[] num) {
 		int vote = num[0];
-	    int count = 1;
-	    int size = num.length;
-	    //vote from the second number
-	    for( int i = 1; i < size; i++ )
-	    {
-	        if( count == 0 ) { vote = num[i]; count++; }
-	        else if( vote == num[i] )   count++;
-	        else count--;
-	    }
-	    return vote;
+		int count = 1;
+		int size = num.length;
+		// vote from the second number
+		for (int i = 1; i < size; i++) {
+			if (count == 0) {
+				vote = num[i];
+				count++;
+			} else if (vote == num[i])
+				count++;
+			else
+				count--;
+		}
+		return vote;
+	}
+
+	public int method1Pass(int[] num) {
+		int n = num.length;
+		int cur = 0;
+		int majority = 0, majorCount = 0;
+		int[] tmp = new int[n / 2 + 1];
+		int[] tmpCount = new int[n / 2 + 1];
+		tmp[0] = num[0];
+
+		boolean added = false;
+
+		for (int i = 0; i < n; i++) {
+			added = false;
+			for (int j = 0; j <= cur; j++) {
+				if (num[i] == tmp[j]) {
+					added = true;
+					tmpCount[j]++;
+					break;
+				}
+			}
+			if (!added) {
+				cur++;
+				tmp[cur] = num[i];
+				tmpCount[cur]++;
+			}
+		}
+
+		majority = tmp[0];
+		majorCount = 0;
+		for (int i = 0; i < tmp.length; i++) {
+			if (tmpCount[i] > majorCount) {
+				majorCount = tmpCount[i];
+				majority = tmp[i];
+			}
+		}
+		return majority;
 	}
 }
---
+++ leetcode/src/test/java/ylyan/leetcode/CompareVersionNumbersTest.java
@@ -0,0 +1,17 @@
+package ylyan.leetcode;
+
+public class CompareVersionNumbersTest extends BaseTestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	public void testCompareVersionNumbers(){
+		
+	}
+
+}
